2f03bd7435635c4ca44db7e254ebac00472c24e9
Ehab Ashour <ehabdevel@gmail.com>
2018-08-16 08:38:21 +0200

    updates

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter } from 'react-router-dom';
+import { AUTH_TOKEN } from './constants';
+import { ApolloLink } from 'apollo-link'
+
+import App from './components/App';
+
+const httpLink = createHttpLink({ uri: 'https://api.graph.cool/simple/v1/cjkklsqqw396301562tkf7ksa' })
+
+const middlewareLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareLink.concat(httpLink)
+
+const client = new ApolloClient({ 
+  link: httpLinkWithAuthToken,
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
